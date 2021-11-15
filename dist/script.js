d4d3351ab47874481feeaeee001d977e854363f0
flashh <85707769+flashhRDC@users.noreply.github.com>
2021-11-15 15:34:39 +0100

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,14 @@
+let toggle = document.getElementById('toggleDark')
+let image = document.querySelector('.image')
+
+toggle.addEventListener('click', toggleScheme, true)
+
+function toggleScheme () {
+  if (toggle.getAttribute("aria-checked") == "true") {
+      toggle.setAttribute("aria-checked", "false");
+  } else {
+      toggle.setAttribute("aria-checked", "true");
+  }
+  image.classList.toggle('image-dark')
+  image.classList.toggle('image-light')
+}
